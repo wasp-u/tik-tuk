e529e0c700640595ed092236fdec43a71199253c
“wasp” <osadchiy.u@gmail.com>
2021-11-17 17:11:55 +0200

    add loading animation

--- src/components/FeedPage/FeedPage.jsx
+++ src/components/FeedPage/FeedPage.jsx
@@ -2,123 +2,8 @@ import Post from '../Post/Post';
 import style from './FeedPage.module.css'
 
 const FeedPage = (props) => {
-    let feed = [
-        {
-            "id": "7005926237130657025",
-            "secretID": "7005926237130657025",
-            "text": "This slow version🥵 Inst: kikakim🤍",
-            "createTime": 1631194316,
-            "authorMeta": {
-                "id": "6785823001336415238",
-                "secUid": "MS4wLjABAAAAeH_XfG3mng5HdtOKKaKXj-breE3_2JkVUjlF5REet8fu3MeuaOCoRqNV06xcX_U4",
-                "name": "kikakiim",
-                "nickName": "Kika Kim",
-                "verified": false,
-                "signature": "@xoteam Instagram: @kikakim, Cooperation: kikakim.booking@gmail.com",
-                "avatar": "https://p16-sign-sg.tiktokcdn.com/aweme/1080x1080/tos-alisg-avt-0068/a4777fe51994e2ff798bdc9dd1100846.jpeg?x-expires=1637150400&x-signature=D1QjKGAgWDai63EovNutdvoXB3s%3D",
-                "following": 89,
-                "fans": 24100000,
-                "heart": 628000000,
-                "video": 827,
-                "digg": 6732,
-            },
-            "musicMeta": {
-                "musicId": "6999418832666266374",
-                "musicName": "original sound",
-                "musicAuthor": "xxtristanxo",
-                "musicOriginal": true,
-                "musicAlbum": "",
-                "playUrl": "https://sf16-ies-music-va.tiktokcdn.com/obj/musically-maliva-obj/6999418913268288262.mp3",
-                "coverThumb": "https://p16-sign-va.tiktokcdn.com/musically-maliva-obj/1664736066053125~c5_100x100.jpeg?x-expires=1637150400&x-signature=4AZsRXsDPO%2BctxrWccA1edAcqb0%3D",
-                "coverMedium": "https://p16-sign-va.tiktokcdn.com/musically-maliva-obj/1664736066053125~c5_720x720.jpeg?x-expires=1637150400&x-signature=yvG2kqFsJdR44Ue%2FJuiTfRDhnAI%3D",
-                "coverLarge": "https://p16-sign-va.tiktokcdn.com/musically-maliva-obj/1664736066053125~c5_1080x1080.jpeg?x-expires=1637150400&x-signature=MEldPOT%2FVvKnVGWam1Q8CNw%2F5qY%3D",
-                "duration": 16,
-            },
-            "covers": {
-                "default": "https://p16-sign-sg.tiktokcdn.com/obj/tos-alisg-p-0037/61e38cee7c814bd48e8c1a3bfd00de49_1631194317?x-expires=1637085600&x-signature=l8kfh8DNoPq2%2B%2BdCJi%2BDMGqHIjA%3D",
-                "origin": "https://p16-sign-sg.tiktokcdn.com/obj/tos-alisg-p-0037/5f12071f0c44496e83f3f17cd0a01003_1631194318?x-expires=1637085600&x-signature=vYq8q3y5DNEsHenPIPg1GS3mFsw%3D",
-                "dynamic": "https://p16-sign-sg.tiktokcdn.com/obj/tos-alisg-p-0037/358ef5bb1a0347199dded2a255a27ff5_1631194318?x-expires=1637085600&x-signature=VbU2R2jZXUv91m8DQ0MozKBH5N0%3D",
-            },
-            "webVideoUrl": "https://www.tiktok.com/@kikakiim/video/7005926237130657025",
-            "videoUrl": "https://v77.tiktokcdn.com/f7787bf903a3ccabe1f77c629bbcbe43/61944d0e/video/tos/useast2a/tos-useast2a-pve-0068/644e0a5848f547c09135799d4aa6bff0/?a=1233&br=3454&bt=1727&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=wZ~R_F5qkag3-I&l=20211116182959010189071071412EC979&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=M3R1aWk6ZjZvODMzNzczM0ApOzhlNDhnZDs3N2dpZGc6O2c2aGpmcjRfcmxgLS1kMTZzczEvMmAwLmBjXy0vYTIxMGA6Yw%3D%3D&vl=&vr=",
-            "videoUrlNoWaterMark": "",
-            "videoApiUrlNoWaterMark": "",
-            "videoMeta": {
-                "height": 960,
-                "width": 540,
-                "duration": 15,
-            },
-            "diggCount": 2300000,
-            "shareCount": 10300,
-            "playCount": 16400000,
-            "commentCount": 12100,
-            "downloaded": false,
-            "mentions": [],
-            "hashtags": [],
-            "effectStickers": [{
-                "id": "459476",
-                "name": "Color Selector"
-            }
-            ]
-        },
-        {
-            "id": "7003277860790144262",
-            "secretID": "7003277860790144262",
-            "text": "Das war am selben Tag ALDI ich mach meine Videos mehr an der Kasse 🙃#foryoupage #aldinord #fyfyfyfy #foryoupageofficiall",
-            "createTime": 1630577695,
-            "authorMeta": {
-                "id": "6775980874561471493",
-                "secUid": "MS4wLjABAAAAFWwlr9fp5ZIiczhkdPu7WRYCTG-RI1y4B2HlzYYQu5sjZ65T8BqJ3KiWdQAuzCMn",
-                "name": "elaine.victoria",
-                "nickName": "Elaine Victoria 🕊",
-                "verified": false,
-                "signature": "No bio yet",
-                "avatar": "https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/94c0343f131c3c203edae0001698b10a~c5_1080x1080.jpeg?x-expires=1637172000&x-signature=WR9d6ICX%2BNUi3hkWuxQ6Xzy9iTc%3D",
-                "following": 156,
-                "fans": 888300,
-                "heart": 16200000,
-                "video": 396,
-                "digg": 1421,
-            },
-            "musicMeta": {
-                "musicId": "6936835005490465541",
-                "musicName": "original sound",
-                "musicAuthor": "The Forgotten Songs",
-                "musicOriginal": true,
-                "musicAlbum": "",
-                "playUrl": "https://sf16-ies-music-va.tiktokcdn.com/obj/musically-maliva-obj/6936835018018802438.mp3",
-                "coverThumb": "https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/2ea7599c422bdc58b279979e60488391~c5_100x100.jpeg?x-expires=1637172000&x-signature=gZ95PnCZ75AD9fsc7htQ8XYVlFg%3D",
-                "coverMedium": "https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/2ea7599c422bdc58b279979e60488391~c5_720x720.jpeg?x-expires=1637172000&x-signature=zx5QZuwd14PqOdAsJThkkPCr3YU%3D",
-                "coverLarge": "https://p16-sign-va.tiktokcdn.com/tos-maliva-avt-0068/2ea7599c422bdc58b279979e60488391~c5_1080x1080.jpeg?x-expires=1637172000&x-signature=aULEd%2FRjhEB89IiWsPDHodGrPUo%3D",
-                "duration": 14,
-            },
-            "covers": {
-                "default": "https://p16-sign-va.tiktokcdn.com/obj/tos-maliva-p-0068/711a97099b8a4befa4cbdc123c34cd5d_1630577696?x-expires=1637107200&x-signature=3urZ3%2Fs98hnqqGcNNZSpUhkWq80%3D",
-                "origin": "https://p16-sign-va.tiktokcdn.com/obj/tos-maliva-p-0068/714792f72be04259b6985587c9d75cc3_1630577696?x-expires=1637107200&x-signature=zF9AsoqYRNFXW3ON3CXdAAzKUqQ%3D",
-                "dynamic": "https://p16-sign-va.tiktokcdn.com/obj/tos-maliva-p-0068/00729064e3e94ad79211cfb17271bd80_1630577697?x-expires=1637107200&x-signature=MUKysLpOO9uKJHJGZFnRLJoLLuE%3D",
-            },
-            "webVideoUrl": "https://www.tiktok.com/@elaine.victoria/video/7003277860790144262",
-            "videoUrl": "https://v77.tiktokcdn.com/d14f89782b6c4cac11f6f39208cded53/61944d12/video/tos/useast2a/tos-useast2a-ve-0068c004/c5f6c64a47b04ecd91168fd9d921009c/?a=1233&br=2536&bt=1268&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&ft=wZ~R_F5qkag3-I&l=20211116182959010189071071412EC979&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=amxqNjQ6ZjtkNzMzNzczM0ApMzo6OTo1NGU1Nzk2OzczO2dxc2ZzcjRfNGxgLS1kMTZzc2BeMjQxNC02XjUwLS8uYjA6Yw%3D%3D&vl=&vr=",
-            "videoUrlNoWaterMark": "",
-            "videoApiUrlNoWaterMark": "",
-            "videoMeta": {
-                "height": 1024,
-                "width": 576,
-                "duration": 11,
-            },
-            "diggCount": 1400000,
-            "shareCount": 27600,
-            "playCount": 25900000,
-            "commentCount": 17300,
-            "downloaded": false,
-            "mentions": [],
-            "hashtags": [],
-            "effectStickers": [],
-        }
-    ]
-
+    let posts = []
     if (props.feed.length === 0) {
-        props.setFeed(feed)
         let axios = require("axios").default;
         let options = {
             method: 'GET',
@@ -130,22 +15,20 @@ const FeedPage = (props) => {
         };
 
         axios.request(options).then(function (response) {
-            console.log(response.data);
             props.setFeed(response.data);
-            debugger;
         }).catch(function (error) {
             console.error(error);
         });
-
+        posts = [<img src="https://cdn.dribbble.com/users/902865/screenshots/4814970/loading-opaque.gif" alt="" />]
+    } else {
+        posts = props.feed.map(f => <Post
+            userName={f.authorMeta.nickName}
+            userAvatar={f.authorMeta.avatar}
+            videoUrl={f.videoUrl}
+            likesCount={f.diggCount}
+            commentsCount={f.commentCount}
+            text={f.text} />)
     }
-    let posts = props.feed.map(f => <Post
-        userName={f.authorMeta.nickName}
-        userAvatar={f.authorMeta.avatar}
-        videoUrl={f.videoUrl}
-        likesCount={f.diggCount}
-        commentsCount={f.commentCount}
-        text={f.text} />)
-
     return (
         <div className={style.feed_page_wrapper}>
             {posts}
